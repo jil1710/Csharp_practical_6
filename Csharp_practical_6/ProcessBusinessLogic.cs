04b87f40097ebbc66c90b9da17250b6fafc58eb9
rider <jil.p@simformsolutions.com>
2023-05-08 10:42:39 +0530

    Add project files.

---
+++ Csharp_practical_6/ProcessBusinessLogic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_practical_6
+{
+    public class ProcessBusinessLogic
+    {
+        public event EventHandler? ProcessComplete;
+
+        public void StartProcess()
+        {
+            Console.WriteLine("Process Started...Wait For Completion....");
+            Thread.Sleep(3000);
+            OnProcessComplete(EventArgs.Empty); 
+        }
+
+        protected virtual void OnProcessComplete(EventArgs e)
+        {
+            if (ProcessComplete != null)
+            {
+                ProcessComplete(this, e);
+            }
+        }
+    }
+}
---
+++ Csharp_practical_6/Program.cs
@@ -0,0 +1,16 @@
+﻿using Csharp_practical_6;
+
+class Program : ProcessBusinessLogic
+{
+    public static void Main(string[] args)
+    {
+        Program program = new();
+        program.ProcessComplete += bl_ProcessCompleted;
+        program.StartProcess();
+    }
+
+    public static void bl_ProcessCompleted(object sender , EventArgs e)
+    {
+        Console.WriteLine("Hurray! Process Completed !");
+    }
+}
